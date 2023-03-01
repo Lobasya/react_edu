36a9c0a0dba41e16706c3456f93b272d06769b4b
Danil <danylo_naidonov@epam.com>
2023-03-01 22:07:25 +0200

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,99 @@
 import './App.css';
-import React, { useState } from 'react';
-import User from './User';
-import Modal from './Modal';
+import React, { useState, useEffect } from 'react';
 
-const usersData = [
-  {
-    id: 1,
-    name: 'John',
-    gender: 'male'
-  },
-  {
-    id: 2,
-    name: 'Mila',
-    gender: 'female'
-  },
-  {
-    id: 3,
-    name: 'Huan',
-    gender: 'male'
-  },
-  {
-    id: 4,
-    name: 'Jane',
-    gender: 'female'
-  },
-  {
-    id: 5,
-    name: 'Tom',
-    gender: 'male'
-  },
-];
 
-const App = () => {
-  const [users, setUsers] = useState(usersData);
-  const [isShowModal, setIsShowModal] = useState(false);
-  const [modalUser, setModalUser] = useState(null);
+const App1 = () => {
+    const [counter, setCounter] = useState(0);
+    const [disable, setDisable] = useState(false);
 
-  const filterUsersByName = (name) => {
-    const nameToLowerCase = name.toLowerCase();
-    return usersData.filter(user => user.name.toLowerCase().includes(nameToLowerCase));
-  }
+    useEffect(() => {
+      setInterval(() => {
+        setCounter(prevState => prevState + 1)
+      }, 1000)
+      return () => {}
+    }, [disable]);
 
-  const handleInputChange = (event) => {
-    const filtredUsers = filterUsersByName(event.target.value);
-    setUsers(filtredUsers);
-  }
+    const handleClick = () => {
+      setDisable(!disable);
+    }
 
-  const handleShowModal = (user) => {
-    setIsShowModal(true);
-    setModalUser(user);
-  }
+    return (
+      <div>
+        <h1>{counter}</h1>
+        <button onClick={handleClick}>Clear</button>
+      </div>
+    )
+}
 
-  const handleCloseModal = () => {
-    setIsShowModal(false);
-    setModalUser(null);
-  }
+const Mobile = () => {
+  const [counter, setCounter] = useState(0);
+  const [size, setSize] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      console.log('Mobile')
+      setCounter(prevState => prevState + 1)
+    }, 1000)
+    return () => clearInterval(id);
+  }, []);
+
+  useEffect(() => {
+    const callback = () => {
+      setSize(window.innerWidth);
+      console.log('Mobile resize');
+    }
+    window.addEventListener('resize', callback);
+    return () => window.removeEventListener('resize', callback);
+  }, [])
 
   return (
-    <>
-      {isShowModal ? <Modal handleCloseModal={handleCloseModal} user={modalUser}/> : null}
-      <input type="text" placeholder='Enter name' onChange={handleInputChange}/>
-      <div className='container'>
-        {users.map(user => <User user={user} handleShowModal={() => handleShowModal(user)}/>)}
-      </div>
-    </>
+    <div>
+      <h1>Mobile: {counter}</h1>
+      <h1>Size: {size} px</h1>
+    </div>
+  )
+}
+
+const Desktop = () => {
+  const [counter, setCounter] = useState(0);
+  const [size, setSize] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      console.log('Desk')
+      setCounter(prevState => prevState + 1)
+    }, 1000)
+    return () => clearInterval(id);
+  }, []);
+
+  useEffect(() => {
+    const callback = () => {
+      setSize(window.innerWidth);
+      console.log('Desk resize');
+    };
+    window.addEventListener('resize', callback);
+    return () => window.removeEventListener('resize', callback);
+  }, [])
+
+  return (
+    <div>
+      <h1>Desktop: {counter}</h1>
+      <h1>Size: {size} px</h1>
+    </div>
+  )
+}
+
+const App = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 500);
+
+  useEffect(() => {
+    window.addEventListener('resize', () => setIsMobile(window.innerWidth <= 500))
+  }, [])
+
+  return (
+    <div>
+      {isMobile ? <Mobile/> : <Desktop/>}
+    </div>
   )
 }
 
